3b2de1226f7a1a9e3683ddbb47ff4e29f1212e99
KavindaSandamal <info.kavindasandamal@gmail.com>
2023-07-16 21:18:18 +0530

    First Commit

---
+++ EG_2020_4190/StudentManagementSystem/Model/MainWindowVM.cs
@@ -0,0 +1,111 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Desktop01;
+using Desktop01.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Desktop01
+{
+    public partial class MainWindowVM : ObservableObject
+    {
+        [ObservableProperty]
+        public ObservableCollection<User> users;
+        [ObservableProperty]
+        public User selected = null;
+
+
+
+        public void CloseMainWindow()
+        {
+            Application.Current.MainWindow.Close();
+        }
+
+
+
+
+        [RelayCommand]
+        public void showmessage()
+        {
+
+            MessageBox.Show($"{selected.FirstName} GPA value must be between 0 and 4.", "Error");
+        }
+
+        [RelayCommand]
+        public void AddStudent()
+        {
+            var vm = new AddUserVM();
+            vm.title = "STUDENT REGISTRATION";
+            AddUserWindow window = new AddUserWindow(vm);
+            window.ShowDialog();
+
+            if (vm.Student.FirstName != null)
+            {
+                users.Add(vm.Student);
+            }
+        }
+
+        [RelayCommand]
+        public void DeleteStudent()
+        {
+            if (selected != null)
+            {
+                string name = selected.FirstName;
+                users.Remove(selected);
+                MessageBox.Show($"{name} is Deleted successfuly.", "Delected");
+
+            }
+            else
+            {
+                MessageBox.Show("Plese Select Student before Delete.", "Error");
+
+
+            }
+        }
+        [RelayCommand]
+        public void EditStudent()
+        {
+            if (selected != null)
+            {
+                var vm = new AddUserVM(selected);
+                vm.title = "EDIT USER";
+                var window = new AddUserWindow(vm);
+
+                window.ShowDialog();
+
+
+                int index = users.IndexOf(selected);
+                users.RemoveAt(index);
+                users.Insert(index, vm.Student);
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("Please Select the student", "Error");
+            }
+        }
+
+        public MainWindowVM()
+        {
+            users = new ObservableCollection<User>();
+            BitmapImage image1 = new BitmapImage(new Uri("/Model/Images/7.png", UriKind.Relative));
+            users.Add(new User(12, "Telana", "Harshi", "29/1/2022", image1, 3.0));
+            BitmapImage image2 = new BitmapImage(new Uri("/Model/Images/10.png", UriKind.Relative));
+            users.Add(new User(32, "Chamasha", "Madushani", "1/1/2008", image2, 3.5));
+            BitmapImage image3 = new BitmapImage(new Uri("/Model/Images/3.png", UriKind.Relative));
+            users.Add(new User(23, "Kavinda", "Sandamal", "12/11/1999", image3, 2.8));
+            BitmapImage image4 = new BitmapImage(new Uri("/Model/Images/4.png", UriKind.Relative));
+            users.Add(new User(20, "Esala", "Abishek", "03/10/2002", image4, 2.5));
+
+        }
+    }
+}
+
